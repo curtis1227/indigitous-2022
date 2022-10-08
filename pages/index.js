71ccbe258c05b9152a2736254f2c1ab77decf150
Kshoes <kx360@nyu.edu>
2022-10-08 13:50:08 -0400

    front-end prototype

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,101 @@
-import { useState } from 'react';
+// import { useCallback , useRef, useState } from 'react';
+// import Link from 'next/link';
 
-function createBulletPoint(key, text) {
-  return <li key={key}>{text}</li>;
-}
+// function createBulletPoint(key, text) {
+//   return <li key={key}>{text}</li>;
+// }
 
-export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+// export default function HomePage() {
 
-  const [likes, setLikes] = useState(0);
+//   const searchRef = useRef(null);
+//   const [query, setQuery] = useState('');
+//   const [active, setActive] = useState(false);
+//   const [results, setResults] = useState([]);
 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
+//   const searchEndpoint = (query) => '/api/search?q=${query}'
 
-  return (
-    <div>
-      <h1>Search churches</h1>
-      <ul>
-        {names.map(name => createBulletPoint(name, name))}
-      </ul>
+//   const onChange = useCallback((event) => {
+//     const query = event.target.value;
+//     setQuery(query);
+//     if(query.length) {
+//       fetch(searchEndpoint(query))
+//         .then(res => res.json())
+//         .then(res => {
+//           setResults(res.results);
+//         })
+//     }
+//     else {
+//       setResults([]);
+//     }
+//   }, []);
 
-      <button onClick={handleClick}>Like ({likes})</button>
-    </div>
-  );
+//   const onFocus = useCallback(() => {
+//     setActive(true);
+//     window.addEventListener('click', onClick);
+//   }, []);
+
+//   const onClick = useCallback((event) => {
+//     if(searchRef.current && !searchRef.current.contains(event.target)) {
+//       setActive(false);
+//       window.removeEventListener('click', onClick);
+//     }
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Search churches</h1>
+
+//       <input 
+//       value={query}
+//       /> 
+//       { active && results.length > 0 && (
+
+//         <ul>
+//           {results.map(({ id, title }) => (
+//             <li>
+//               <Link href= '/posts/[id]' as={'/posts/${id}'}>
+//                 <a>{title}</a>
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+
+//         )
+//       }
+
+//     </div>
+//   );
+// }
+
+
+
+// Importing modules
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits }
+	from "react-instantsearch-dom";
+import SearchBar from "./components/searchbar.js";
+// require('dotenv-webpack');
+// console.log(process.env);
+
+const searchClient = algoliasearch(
+	'APPLICATION_API_KEY',
+  	'SEARCH_ONLY_API_KEY',
+);
+
+export default function CustomizedSearch() {
+return (
+	<>
+  <h1>Search churches</h1>
+	<InstantSearch
+		searchClient={searchClient}
+		indexName="gfg_dev">
+
+		{/* Adding Search Box */}
+		<SearchBar/>
+
+		{/* Adding Data */}
+		<Hits />
+	</InstantSearch>
+	</>
+);
 }
